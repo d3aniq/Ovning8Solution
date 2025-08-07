9f434e857b5aa46434910e63a079366e2c01b9ce
Jasmin Dzanic <jasmin@dzanic.se>
2025-08-07 13:16:52 +0200

    Works with Postman

--- MovieApi/Program.cs
+++ MovieApi/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using Movie.Data;
 using Microsoft.OpenApi.Models;
+using Movie.Data;
+using Movie.Data.Extensions;         // för AddDataAccess
+using Movie.Data.Mapping;            // för MappingProfile
+using Movie.Services;                // för ServiceManager och mappning
+using Movie.Service.Contracts;        // <-- Lägger till ISeviceManager
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +19,21 @@ builder.Services.AddControllers();
 // Lägg till swagger (om du använder det)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// registrera databasen och UnitOfWork; faller tillbaka på in?memory om ingen connection?string finns
+builder.Services.AddDataAccess(builder.Configuration);
+
+// registrera tjänstelagret (ServiceManager skapar IMovieService, IActorService, m.m.)
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+
+// registrera AutoMapper med din MappingProfile
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+// se till att controllers i Movie.Presentation laddas in
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Movie.Presentation.Controllers.MoviesController).Assembly);
+
 
 var app = builder.Build();
 
